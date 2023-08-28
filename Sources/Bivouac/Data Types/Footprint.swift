404597428fd3511c1dd0514e068524bc73036520
Zack Brown <zack@zackbrown.co.uk>
2023-08-28 15:46:30 +0100

    Add grid footprint and predefined areas

--- Sources/Bivouac/Data Types/Footprint.swift
+++ Sources/Bivouac/Data Types/Footprint.swift
@@ -13,10 +13,66 @@ extension Grid {
     
     public struct Footprint {
         
-        let coordinate: Coordinate
+        public let origin: Coordinate
         
-        let triangles: [Triangle]
+        public let coordinates: [Coordinate]
         
-        let rotation: Axis
+        public let rotation: Axis
+        
+        init(origin: Coordinate,
+             coordinates: [Coordinate],
+             rotation: Axis = .x) {
+            
+            self.origin = origin
+            self.coordinates = coordinates.rotate(toward: rotation).map { origin + $0 }
+            self.rotation = rotation
+        }
+        
+        init(origin: Coordinate,
+             area: Area,
+             rotation: Axis = .x) {
+            
+            self.init(origin: origin,
+                      coordinates: area.footprint,
+                      rotation: rotation)
+        }
+    }
+}
+
+extension Grid.Footprint {
+    
+    public enum Area {
+        
+        case triangle
+        case trianglePerimeter
+        case rhombus
+        case star
+        case hexagon
+        case hexagonPerimeter
+        case trapezium
+        
+        public var footprint: [Coordinate] {
+            
+            let origin = Grid.Triangle(.zero)
+            let xAdjacent = Grid.Triangle(origin.adjacent(along: .x))
+            let xDiagonal = Grid.Triangle(origin.diagonal(along: .x))
+            
+            switch self {
+                
+            case .triangle: return [origin.coordinate]
+            case .trianglePerimeter: return [origin.coordinate] + origin.adjacent
+            case .rhombus: return [origin.coordinate,
+                                   xAdjacent.coordinate]
+            case .star: return origin.adjacent + xAdjacent.adjacent
+            case .hexagon: return [origin.coordinate,
+                                   origin.adjacent(along: .y),
+                                   origin.adjacent(along: .z),
+                                   xDiagonal.coordinate,
+                                   xDiagonal.adjacent(along: .y),
+                                   xDiagonal.adjacent(along: .z)]
+            case .hexagonPerimeter: return Array(Set(Area.hexagon.footprint.flatMap { Grid.Triangle($0).perimeter }))
+            case .trapezium: return [origin.coordinate] + origin.perimeter
+            }
+        }
     }
 }
